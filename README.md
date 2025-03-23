670fd75b2d1da2ce490997ebdd1d731853bea5f7
stevens456787 <stevenstrudient@gmail.com>
2025-03-23 16:39:35 +0300

    update the models.py for users

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # WeatherApi
 
 # am creating a weather api for my capstone project
+# added new apps like weather, locations, users, alerts and visualization to the capstone project
+
+# good progress
+
--- weather_api/locations/models.py
+++ weather_api/locations/models.py
@@ -1,10 +1,2 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-import uuid
-
 # Create your models here.
-class CustomUser(AbstractUser):
-    role = models.CharField(max_length=100, default='user', choices=[('user', 'User'), ('admin', 'Admin'), ('staff', 'Staff')])
-    api_key = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now=True)
