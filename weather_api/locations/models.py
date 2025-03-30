c660a9bd6e8c1d7f0fcf07d84421b73a5e383aca
stevens456787 <stevenstrudient@gmail.com>
2025-03-30 15:29:16 +0300

    credated models.py for users and location

--- weather_api/locations/models.py
+++ weather_api/locations/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+from users.models import CustomUser
+
+
 # Create your models here.
+
+class Location(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='locations')
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
