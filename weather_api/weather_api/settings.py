27bc51952d14e3563bfc6d6b02e1c8d2ae22847e
stevens456787 <stevenstrudient@gmail.com>
2025-03-31 11:42:14 +0300

    update database

--- weather_api/weather_api/settings.py
+++ weather_api/weather_api/settings.py
@@ -84,9 +84,13 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "ronaldo",
+        "HOST": "127.0.0.1" ,
+        "PORT": "5432",                # Replace with your database port
+        }
 }
 
 
